9d2672d069cfe968d86d1b40e2b6061be1658553
shjshldygw <kxxhmr@gmail.com>
2020-01-30 15:22:03 +0800

    committed by cx

--- assets/fancybox/dist/jquery.fancybox.js
+++ assets/fancybox/dist/jquery.fancybox.js
@@ -167,7 +167,7 @@
     //   "zoom-in-out'
     //   "rotate'
     //
-    transitionEffect: "fade",
+    transitionEffect: false,
 
     // Duration in ms for transition animation
     transitionDuration: 366,
